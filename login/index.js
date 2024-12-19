88d1bafa65e6d6a9468f6e424a77a2e6cd713fe8
NurbekLDM <jumaboyevabarno80@gmail.com>
2024-12-19 17:02:34 +0500

    Initial commit

--- login/index.js
+++ login/index.js
@@ -1,18 +1,22 @@
 const url = 'https://6763a87617ec5852cae98323.mockapi.io/users';
 
 
-function login(){
+function login(event){
+    event.preventDefault();
 const login_username = document.querySelector('.login-username').value
 const login_password = document.querySelector('.login-password').value
 
 axios.get(url)
 .then(function(res){
     const data = res.data
-    if(login_username === data.username && login_password === data.password ){
-        window.location.href = '417-group-frontend-mkdj.vercel.app'
-    }else{
-        alert('Username or password is incorrect')
-    }
+    data.find(function(user){
+        if(login_username === user.username & login_password == user.password ){
+            window.location.href = 'https://417-group-frontend-mkdj.vercel.app'
+        }else{
+            document.getElementById('error').innerHTML = 'Username or password is incorrect'
+        }
+    })
+
 
 })
 }
